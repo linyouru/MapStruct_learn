ba4d10cae2c0b62ed40515bee4ec85054181b5c2
linyouru1 <linyouru@zlg.cn>
2023-04-25 04:37:40 +0800

    Initial commit

---
+++ src/main/java/com/lyr/mapstruct_learn/MapStructLearnApplication.java
@@ -0,0 +1,13 @@
+package com.lyr.mapstruct_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapStructLearnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapStructLearnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lyr/mapstruct_learn/MapStructLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lyr.mapstruct_learn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
